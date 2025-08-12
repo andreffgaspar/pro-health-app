e1be1730c60b893d0da446590e954e0d1d8b2859
andré gaspar <andreffgaspar@hotmail.com>
2025-08-12 09:59:22 -0300

    build

---
+++ build/assets/index-DY66eOsm.js
@@ -0,0 +1,1 @@
+import{r}from"./index-BQk15iUP.js";const e=r("HealthPlugin",{});export{e as Health};
---
+++ build/assets/index-pjIg4xdT.js
@@ -0,0 +1,1 @@
+import{r as i}from"./index-BQk15iUP.js";const t=i("PushNotifications",{});export{t as PushNotifications};
